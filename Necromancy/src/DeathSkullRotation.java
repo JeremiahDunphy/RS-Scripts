464c7e91de24796e16c298d2c531a467b183d3bd
Jeremiah Dunphy <miahedunphy@gmail.com>
2024-01-27 17:08:21 -0500

    updated script logic, Modified the delay calculation logic in the DeathSkullRotation script.

---
+++ Necromancy/src/DeathSkullRotation.java
@@ -0,0 +1,68 @@
+package src;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DeathSkullRotation {
+
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        int rotationTimes = 0;
+
+        // List of key events and their associated delays
+        List<Map.Entry<Integer, Integer[]>> keyEvents = new ArrayList<>();
+
+        // An Array of delay times to replicate random button spamming.
+        // Total delay divided by 4
+        int baseDelay = 1900 / 4;
+
+        Integer[] delays = {
+                baseDelay + (int) (Math.random() * 26),  // Part of delay, randomized
+                baseDelay + (int) (Math.random() * 26),
+                baseDelay + (int) (Math.random() * 26),
+                baseDelay + (int) (Math.random() * 26)
+        };
+
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_4, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_3, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_2, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_1, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_3, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_5, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_2, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_F1, delays)); //super adrenaline
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_4, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_1, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_6, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_9, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_1, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_1, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_9, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_4, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_2, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_1, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_1, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_6, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_9, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_1, delays));
+        keyEvents.add(new SimpleEntry<>(KeyEvent.VK_4, delays));
+
+        Robot robot = new Robot();
+
+
+        // Loops through the List and presses each key, then waits a random amount of time between 2,000 and 2100 MS.
+        while (rotationTimes < 20) {
+            for (Map.Entry<Integer, Integer[]> keyEvent : keyEvents) {
+                for (int delay : keyEvent.getValue()) {
+                    robot.keyPress(keyEvent.getKey());
+                    robot.keyRelease(keyEvent.getKey());
+                    Thread.sleep(delay);
+                }
+                rotationTimes++;
+            }
+        }
+    }
+}
